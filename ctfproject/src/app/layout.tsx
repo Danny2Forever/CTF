a7f3391aed29c7ef5b38f70fdf22d4a6b442131f
Anner <aruchakhem@gmail.com>
2025-03-11 17:10:27 +0700

    Added Shadcn, framer-motion, and  animation library

--- ctfproject/src/app/layout.tsx
+++ ctfproject/src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner"
+import AOSProvider from '../components/AOSProvider';
+
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
-// Initialize Poppins font with the weights you need
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
@@ -14,7 +16,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Furagu",
-  description: "Furagu application",
+  description: "Furagu Capture The Flag (CTF) Learning Platform Application",
 };
 
 export default function RootLayout({
@@ -25,9 +27,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} font-sans`}>
+        <AOSProvider>
         <Navbar />
         {children}
         <Footer />
+        </AOSProvider>
+        <Toaster
+          theme="light"
+          richColors
+          className="w-[500px]"
+          toastOptions={{
+            style: {
+              fontSize: '10px',
+              minWidth: 'fit-content'
+            },
+            classNames: {
+              toast: 'font-line-seed',
+              title: 'text-xs md:text-lg whitespace-nowrap overflow-hidden text-ellipsis',
+              description: 'text-sm',
+            },
+          }}
+        />
       </body>
     </html>
   );
