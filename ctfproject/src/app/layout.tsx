3fb092e2576005c04ce1fa48526bc7ccdd4d466a
KwnxpN <tonnum.kawin@gmail.com>
2025-03-11 16:26:21 +0700

    add footer

--- ctfproject/src/app/layout.tsx
+++ ctfproject/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 // Initialize Poppins font with the weights you need
 const poppins = Poppins({
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} font-sans`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ ctfproject/src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="w-full py-6 mt-auto">
+            <div className="container mx-auto text-center text-gray-600 text-sm">
+                &copy; Furagu Capture The Flag 2025
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
